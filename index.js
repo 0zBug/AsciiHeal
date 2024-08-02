cc4f216ae122003fc79db9f0c3d00c32b25f1367
Bug <77031032+0zBug@users.noreply.github.com>
2024-08-02 12:56:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -18,30 +18,43 @@ function monospacedText(text, font) {
     }
 
     var monospacedString = "";
-    
-    var spaceCharacters = [" ", " ", " "];
-    var spaceWidths = spaceCharacters.map(space => getTextWidth(space, font));
-    
+
+    var spaceCharacters = [
+        { char: "\u200A", width: getTextWidth("\u200A", font) }, 
+        { char: "\u2800", width: getTextWidth("\u2800", font) },
+        { char: "\u2007", width: getTextWidth("\u2007", font) },
+        { char: "\u2003", width: getTextWidth("\u2003", font) }, 
+        { char: "\u2002", width: getTextWidth("\u2002", font) },
+        { char: " ", width: getTextWidth(" ", font) },
+        { char: "\u2004", width: getTextWidth("\u2004", font) }, 
+        { char: "\u2005", width: getTextWidth("\u2005", font) },
+        { char: "\u2008", width: getTextWidth("\u2008", font) },
+        { char: "\u2009", width: getTextWidth("\u2009", font) },
+        { char: "\u2006", width: getTextWidth("\u2006", font) },
+    ];
+
     for (i = 0; i < characters.length; i++) {
         var char = characters[i];
         var charWidth = getTextWidth(char, font);
         var remainingWidth = maxWidth - charWidth;
         var spaceCount = "";
-        
+
         for (var j = 0; j < spaceCharacters.length; j++) {
-            var count = Math.floor(remainingWidth / spaceWidths[j] / 4);
-            remainingWidth -= count * spaceWidths[j];
-            spaceCount += spaceCharacters[j].repeat(count);
+            var space = spaceCharacters[j];
+            var count = Math.floor(remainingWidth / space.width / 4);
+            if (count > 0) {
+                spaceCount += space.char.repeat(count);
+                remainingWidth -= count * space.width;
+            }
         }
 
         monospacedString += char + spaceCount;
     }
 
-    return  "⠀\n" + monospacedString.replace(/\\/g, "\\\\").replace(/\|/g, "\\|").replace(/_/g, "\\_").replace(/`/g, "\\`").replace(/~/g, "\\~");
+    return "\u2800\n" + monospacedString.replace(/\\/g, "\\\\").replace(/\|/g, "\\|").replace(/_/g, "\\_").replace(/`/g, "\\`").replace(/~/g, "\\~").replace(/\*/g, "\\*");
 }
 
 document.getElementById("encode").addEventListener("click", () => {
     output.innerHTML = monospacedText(input.value, font.value);
-    output.font = font.value
     characterAmount.innerHTML = output.textContent.length + " chars";
 });
